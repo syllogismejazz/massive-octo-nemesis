cdd2f43701cf40ae4f7a066622ca77535203d7e3
syllogismejazz <MarcAndreSeguin@Marc-Andres-MacBook-Air.local>
2013-09-27 09:54:05 -0400

    Removed comments

--- sampling pvalues from mv standard normal.R
+++ sampling pvalues from mv standard normal.R
@@ -7,19 +7,8 @@ sigma = diag(2)
 #gamma = m / m+n so:
 n = ceiling((m/gamma)-m)
 
-#Test using Replicate() instead of loop#
+#Using Replicate() instead of loop#
 p_values = replicate(repeats,K.statistic(rmvnorm(m,mean,sigma), rmvnorm(n,mean,sigma),0.5,0.5)$p_value)
-
-# OLD CODE USING LOOP
-#
-# for(i in 1:repeats){
-# 
-# 	first.bivariate.sample=rmvnorm(m, mean=c(0,0), sigma=diag(2))
-# 	second.bivariate.sample=rmvnorm(n, mean=c(0,0), sigma=diag(2))
-# 	
-# 	p_values[i]=(K.statistic(first.bivariate.sample, second.bivariate.sample, 0.5, 0.5))$p_value
-# 		
-# }
 
 return(p_values)
 	
