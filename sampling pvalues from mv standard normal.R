44d8f4ef92f09efd00717753d2bf3e54995be913
syllogismejazz <MarcAndreSeguin@Marc-Andres-MacBook-Air.local>
2013-09-27 09:03:25 -0400

    Gamma now an argument
    Easier to handle when calling this function.

--- sampling pvalues from mv standard normal.R
+++ sampling pvalues from mv standard normal.R
@@ -1,8 +1,11 @@
-sampling.pvalues.from.mv.standard.normal<- function(repeats=10, m=10, n=10){
+sampling.pvalues.from.mv.standard.normal<- function(m=10, gamma=0.5, repeats=10){
 
 p_values = 0
 mean = c(0,0)
 sigma = diag(2)
+
+#gamma = m / m+n so:
+n = ceiling((m/gamma)-m)
 
 #Test using Replicate() instead of loop#
 p_values = replicate(repeats,K.statistic(rmvnorm(m,mean,sigma), rmvnorm(n,mean,sigma),0.5,0.5)$p_value)
