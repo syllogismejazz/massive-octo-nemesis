1863c009ffd9d134ebb8243bdefdd8cf96ecf80d
syllogismejazz <MarcAndreSeguin@Marc-Andres-MacBook-Air.local>
2013-10-06 16:32:58 -0400

    Sampling Pvalue functions
    now incorporate scalars c_N1 and c_N2

--- sampling pvalues from mv student.R
+++ sampling pvalues from mv student.R
@@ -1,4 +1,4 @@
-sampling.pvalues.from.mv.student<- function(m=10, gamma=0.5, repeats=10){
+sampling.pvalues.from.mv.student<- function(m=10, gamma=0.5, repeats=10, c_N1=0.5, c_N2=0.5){
 
 p_values = 0
 mean = c(0,0)
@@ -8,8 +8,7 @@ sigma = diag(2)
 n = ceiling((m/gamma)-m)
 
 #Using Replicate() instead of loop#
-p_values = replicate(repeats,K.statistic(rmvt(m,sigma, df=4), rmvt(n,sigma, df=4),0.5,0.5)$p_value)
-
+p_values = replicate(repeats,K.statistic(rmvt(m,sigma, df=4), rmvt(n,sigma, df=4),c_N1,c_N2)$p_value)
 
 return(p_values)
 	
